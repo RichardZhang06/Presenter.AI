11526df4d24fb53cc456a933c0481f575a65cab8
Samuel Zhang <samuelwenhaizhang@gmail.com>
2025-02-08 15:14:50 -0500

    Fixed footer easy for 10x dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import PrepPage from "./pages/PrepPage";
 import AboutPage from "./pages/AboutPage";
 import SpeechPage from "./pages/SpeechPage";
-import PrepPage from "./pages/PrepPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 const App = () => {
-    return (
-        <Router>
-            {/* Navigation */}
-            <Navbar />
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
 
-            {/* Page Content */}
-            <div className="container mx-auto p-6">
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/speech" element={<SpeechPage />} />
-                    <Route path="/prep" element={<PrepPage />} />
-                </Routes>
-            </div>
+        {/* Navigation */}
+        <Navbar />
 
-            {/* Footer */}
-            <Footer />
-        </Router>
-    );
+        {/* Page Content */}
+        <div className="container mx-auto p-6 flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/prep" element={<PrepPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/speech" element={<SpeechPage />} />
+          </Routes>
+        </div>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,9 @@
 // src/components/Footer.js
-
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-10">
+    <footer className="bg-gray-800 text-white text-center py-4 mt-6">
       <p>© 2025 Presenter.ai - All Rights Reserved</p>
     </footer>
   );
